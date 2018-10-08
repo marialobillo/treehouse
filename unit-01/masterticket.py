43d1487d613341edf7774335060d9062c18c29aa
marialobillo <maria.lobillo.santos@gmail.com>
2018-10-08 09:45:41 +0200

    handle exceptions

--- unit-01/masterticket.py
+++ unit-01/masterticket.py
@@ -5,39 +5,24 @@ tickets_remaining = 100
 # Run this code continuously
 #
 while tickets_remaining:
-
-    # Output how many tickets are remaining using the tickets_remaining variable
-
     print("There are {} tickets remaining".format(tickets_remaining))
-
-
-    # Gather the use's name and assign it to a new variable
-    name = input("What is your name?")
-
-    # Prompt the user by name and ask how many tickets they would like
-    number_tickets = input("How many tickets you would like, {}? ".format(name))
-    # Calculate the price (number of tickets muliplied by the price) and assign
-    # it to a variable
-    number_tickets = int(number_tickets)
-    price = number_tickets * TICKET_PRICE
-
-
-    # Output the price to the screen
-    print("The total price is {}".format(price))
-
-    # Prompt user if they want to proceedself.
-    answer = input("Do you want to continue? (y/n)")
-
-    # If they want to proceed
-    if(answer == 'y'):
-        print('SOLD!')
-        tickets_remaining = tickets_remaining - number_tickets
+    user_name = input("What is your name?  ")
+    number_tickets = input("How many tickets you would like, {}? ".format(user_name))
+    try:
+        number_tickets = int(number_tickets)
+        if number_tickets > tickets_remaining:
+            raise ValueError("There are only {} tickets".format(tickets_remaining))
+    except ValueError as err:
+        print("Oh no, we ran unto an issue. {}. Please try again".format(err))
     else:
-        print('Thanks {}!'.format(name))
-    # Otherwise
-    #
-    #   thank them by name
-
+        amount_due = number_tickets * TICKET_PRICE
+        print("The total price is {}".format(amount_due))
+        answer = input("Do you want to continue? (y/n)")
+        if(answer.lower() == 'y'):
+            print('SOLD!')
+            tickets_remaining = tickets_remaining - number_tickets
+        else:
+            print('Thanks {}!'.format(name))
 
 # Notify the user that the tickets are sold out.
 print("Sorry, the tickets are all sold out :(")
