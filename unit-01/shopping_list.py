9edee0aeba8c61cc231425890df9efa11a81d95c
marialobillo <maria.lobillo.santos@gmail.com>
2018-10-10 13:38:30 +0200

    showing the shoping list

--- unit-01/shopping_list.py
+++ unit-01/shopping_list.py
@@ -6,12 +6,21 @@ def add_to_list(item):
     shopping_list.append(item)
     print("Added! List has {} items".format(len(shopping_list)))
 
+
 def show_help():
     print("What should we pick up at the store?")
     print("""
 Enter 'DONE' to stop adding items.
 Enter 'HELP' for this help.
+ENTER 'SHOW' for see the complete list.
 """)
+
+
+def show_list():
+    print("Here's your list:")
+    for item in shopping_list:
+        print('* ' + item)
+
 
 show_help()
 
@@ -23,6 +32,11 @@ while True:
     elif new_item == 'HELP':
         show_help()
         continue
+    elif new_item == 'SHOW':
+        show_list()
+        continue
 
     # call add_to_list with new_item as an argument
     add_to_list(new_item)
+
+show_list()
