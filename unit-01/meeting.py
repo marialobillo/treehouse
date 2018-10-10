60786688a6cf2137bcc49ebbd7da0949f72f7e58
marialobillo <maria.lobillo.santos@gmail.com>
2018-10-10 13:14:24 +0200

    meeting

--- unit-01/meeting.py
+++ unit-01/meeting.py
@@ -1,3 +1,13 @@
 attendees = ["Ken", "Alena", "Sam"]
+attendees.append("Ashley")
+attendees.exnted(["James", "Guil"])
+optional_invitees = ["Ben J.", "Dave"]
+potential_attendees = attendees + optional_invitees
+print(potential_attendees)
 
 print("There are", len(attendees), "attendees currently")
+
+to_line = ", ".join(attendees)
+cc_line = ", ".join(optional_invitees)
+print("To: " + to_line)
+print("Cc: " + cc_line)
---
+++ unit-01/shopping_list.py
@@ -0,0 +1,27 @@
+# Create a new empty list named shopping_list
+shopping_list = []
+
+# create a function named add_to_list that declares a parameter named item
+def add_to_list(item):
+    shopping_list.apend(item)
+
+def show_help():
+    print("What should we pick up at the store?")
+    print("""
+Enter 'DONE' to stop adding items.
+Enter 'HELP' for this help.
+""")
+
+show_help()
+
+while True:
+    new_item = input("> ")
+
+    if new_item == 'DONE':
+        break
+    elif new_item == 'HELP':
+        show_help()
+        continue
+
+    # call add_to_list with new_item as an argument
+    add_to_list(new_item)
